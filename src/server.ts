c130d31da4faeefce47818df0e419c48cde38e2b
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-05-19 18:13:53 -0300

    Config static files

--- src/server.ts
+++ src/server.ts
@@ -4,22 +4,35 @@ import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import multipart from '@fastify/multipart'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'path'
 
 const app = fastify()
 
 app.register(cors, {
   origin: true,
 })
+
+app.register(multipart)
+
 app.register(jwt, {
   secret: 'spacetime',
 })
 
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
+
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('listening on port 3333')
