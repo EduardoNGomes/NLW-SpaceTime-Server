fb081f58e9c7b01bf72d80055060bebd67cb0907
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-06-03 14:50:34 -0300

    add cloudinary on upload route

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,51 +1,42 @@
 import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
-import { createWriteStream } from 'node:fs'
+import { createWriteStream, unlinkSync } from 'node:fs'
 import { extname, resolve } from 'node:path'
 import { pipeline } from 'node:stream'
 import { promisify } from 'node:util'
+import { createImageUrl } from '../utils/upload-cloud'
 
 const pump = promisify(pipeline)
 
 export async function uploadRoutes(app: FastifyInstance) {
-  // app.post('/upload', async (request, reply) => {
-  //   const upload = await request.file({
-  //     limits: {
-  //       fieldSize: 5_242_880, // 5mb
-  //     },
-  //   })
+  app.post('/upload', async (request, reply) => {
+    const upload = await request.file({
+      limits: {
+        fieldSize: 5_242_880, // 5mb
+      },
+    })
+    if (!upload) {
+      return reply.status(400).send()
+    }
+    const mimetypeRegex = /^(image|video)\/[a-zA-Z]+/
+    const isValidFileFormat = mimetypeRegex.test(upload.mimetype)
+    if (!isValidFileFormat) {
+      return reply.status(400).send()
+    }
+    const fileId = randomUUID()
+    const extension = extname(upload.filename)
+    const fileName = fileId.concat(extension)
+    const writeStream = createWriteStream(
+      resolve(__dirname, '../../uploads', fileName),
+    )
+    await pump(upload.file, writeStream)
 
-  //   if (!upload) {
-  //     return reply.status(400).send()
-  //   }
+    const response = await createImageUrl(
+      resolve(__dirname, '../../uploads', fileName),
+    )
 
-  //   const mimetypeRegex = /^(image|video)\/[a-zA-Z]+/
+    unlinkSync(resolve(__dirname, '../../uploads', fileName))
 
-  //   const isValidFileFormat = mimetypeRegex.test(upload.mimetype)
-
-  //   if (!isValidFileFormat) {
-  //     return reply.status(400).send()
-  //   }
-
-  //   const fileId = randomUUID()
-
-  //   const extension = extname(upload.filename)
-  //   console.log(extension)
-
-  //   const fileName = fileId.concat(extension)
-  //   console.log(fileName)
-
-  //   const writeStream = createWriteStream(
-  //     resolve(__dirname, '../../uploads', fileName),
-  //   )
-
-  //   await pump(upload.file, writeStream)
-
-  //   const fullUrl = request.protocol.concat('://').concat(request.hostname)
-  //   console.log(fullUrl)
-  //   const fileUrl = new URL(`uploads/${fileName}`, fullUrl).toString()
-
-  //   return { fileUrl }
-  // })
-  app.post('/upload', async (request, reply) => {})
+    return { fileUrl: response?.url }
+  })
 }
